1a7011b2d476c86055578126c6e13683e153aa10
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-06 04:20:10 -0600

    having too much fun in css to focus on data lol

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('build'));
