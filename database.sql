a65475c3610aa4c31f8f440c05d82857c20e6fcc
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-06 14:35:53 -0600

    added sql file

---
+++ database.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE "people" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (120) NOT NULL,
+    "ethnicity" VARCHAR (120) ,
+    "education" VARCHAR (120) ,
+    "gender" VARCHAR (120),
+    "language" VARCHAR (120)
+);
