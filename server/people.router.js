3b047062dbb8f11d6e8e0319f0cc18ddd133818b
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-06 21:41:42 -0600

    adding items to database but map function prevents catch from working correctly

--- server/people.router.js
+++ server/people.router.js
@@ -4,28 +4,31 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const people = req.body;
-  const insertPeople = `INSERT INTO "people" 
-   ("name" ,
-    "ethnicity" , 
-    "education" , 
-    "gender", 
-    "language");`;
-  pool
-    .query(insertPeople, [
-      people.name,
-      people.ethnicity,
-      people.education,
-      people.gender,
-      people.language,
-    ])
-    .then(() => {
-      res.sendStatus(201);
-      console.log(`${people} added to database`);
-    })
-    .catch((error) => {
-      console.log(`Problem adding people to database, ${error}`);
-      res.sendStatus(500);
-    });
+  const insertPeople = `
+    INSERT INTO "people" 
+     ("name",
+      "ethnicity", 
+      "education", 
+      "gender", 
+      "language")
+      VALUES ($1, $2, $3, $4, $5);`;
+  people.map((person) => {
+    pool
+      .query(insertPeople, [
+        person.name,
+        person.ethnicity,
+        person.education,
+        person.gender,
+        person.language,
+      ])
+      .then((result) => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log(`Problem adding people to database, ${error}`);
+        res.sendStatus(500);
+      });
+  });
 });
 
 module.exports = router;
