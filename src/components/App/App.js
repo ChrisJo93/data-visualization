e4e63b66437aca9bca4b95a306e125669d9b94ff
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-09 02:51:12 -0600

    sourced in graph component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import Graph from '../Graph/Graph';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
     <div className="App box">
       <p>Howdy</p>
       <p>{count}</p>
+      <Graph />
       <div className="buttonBox">
         <button onClick={() => setCount(count + 1)}>Click Me</button>
         <button onClick={() => setCount(count - 1)}>Click Me</button>
