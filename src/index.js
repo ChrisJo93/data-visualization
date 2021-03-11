264e061fe9d9779f654319a5ee9c412899c76894
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-10 22:00:11 -0600

    right before removing strictmode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+import rootReducer from './redux/Reducers/_root.reducer';
+import rootSaga from './redux/Sagas/_root.saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWareList =
+  process.env.NODE_ENV === 'development'
+    ? [sagaMiddleware, logger]
+    : [sagaMiddleware];
+
+const store = createStore(rootReducer, applyMiddleware(...middleWareList));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
