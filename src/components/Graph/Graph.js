4e19e6d56f1a7960e1e2b97ec93676b10d3a8bee
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-03-11 18:59:23 -0600

    store is accessed correctly and useEffect loop is solved

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -3,19 +3,20 @@ import { connect, useDispatch } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import axios from 'axios';
 
-function Graph() {
+function Graph(props) {
   const [people, setPeople] = useState('');
   const dispatch = useDispatch();
 
   useEffect(() => {
     // peopleList();
-    // dispatch({
-    //   type: 'GET_PEOPLE',
-    // });
-  }, []);
+    dispatch({
+      type: 'GET_PEOPLE',
+    });
+    setPeople(props.store.graphR.join(''));
+    console.log(people);
+  }, [dispatch, props.store.graphR, people]);
 
-  //Axios call for people list within Graph file. Using redux is overkill
-  //but good practice.
+  //Axios call for people list within Graph file. Using redux for practice.
   /* const peopleList = () =>
       axios
         .get(`/api/people`)
@@ -25,10 +26,14 @@ function Graph() {
         .catch((error) => {
           console.log(error);
           });  */
+  const stupid = Array.from(people);
+  const stupid2 = stupid.map((person) => person.name);
 
   return (
     <div>
-      <button onClick={() => console.log('poop')}>hey girl hey</button>
+      <button onClick={() => console.log(stupid)}>hey girl hey</button>
+      <button onClick={() => console.log(stupid2)}>I am here</button>
+
       <div></div>
     </div>
   );
